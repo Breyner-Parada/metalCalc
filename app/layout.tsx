e29b196b83bd90569a6cf2aa250508190056ed8b
Breyner Parada <breyner@quepar-solutions.com>
2026-02-10 11:56:11 -0500

    Add favicon and update layout to include icon link

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,6 +25,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/metalca.jpeg" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
